3494d094ec7c1f110b5e01bd6fb80470f46d69bf
Ayushman <91052985+ayushman46@users.noreply.github.com>
2026-06-05 13:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.17%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -8,43 +8,47 @@ class Solution(object):
         n = len(matrix[0])
         c = m * n
         count = 0
-        colstart = 0
-        colend = n - 1
-        rowstart = 0
-        rowend = m - 1
+        top = 0
+        r = n - 1
+        l = 0
+        bottom = m - 1
         result = []
 
-        while count < c:
-            # Traverse from left to right along top row
-            for i in range(colstart, colend + 1):
-                result.append(matrix[rowstart][i])
-                count += 1
-            rowstart += 1
-            if count == c:
-                break
-
-            # Traverse from top to bottom along right column
-            for j in range(rowstart, rowend + 1):
-                result.append(matrix[j][colend])
-                count += 1
-            colend -= 1
-            if count == c:
-                break
-
-            # Traverse from right to left along bottom row
-            if rowstart > rowend:  # Prevent invalid traversal
-                break
-            for i in range(colend, colstart - 1, -1):
-                result.append(matrix[rowend][i])
-                count += 1
-            rowend -= 1
-            if count == c:
-                break
+        while count<c:
+            for i in range(l,r+1):
+                result.append(matrix[top][i])
+                count+=1
+                
+            top+=1
+            if count==c:
+                    break
 
 
-            for i in range(rowend, rowstart - 1, -1):
-                result.append(matrix[i][colstart])
-                count += 1
-            colstart += 1
+            for i in range(top,bottom+1):
+                result.append(matrix[i][r])
+                count+=1
+                
+               
+            r-=1
+            if count==c:
+                    break
 
+
+            for i in range(r,l-1,-1):
+                result.append(matrix[bottom][i])
+                count+=1
+                
+            bottom-=1
+            if count==c:
+                    break
+
+
+            for i in range(bottom,top-1,-1):
+                result.append(matrix[i][l])
+                count+=1
+                
+                
+            l+=1
+            if count==c:
+                    break
         return result
