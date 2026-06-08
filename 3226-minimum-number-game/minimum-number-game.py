2c6b6afed1c3ae4b8b205c3d09f31e363a223073
Ayushman <91052985+ayushman46@users.noreply.github.com>
2026-06-08 16:44:16 +0530

    Time: 3 ms (53.27%) | Memory: 12.4 MB (17.76%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numberGame(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums=sorted(nums)
+        res=[]
+        for i in range(0,len(nums),2):
+            res.append(nums[i+1])
+            res.append(nums[i])
+        return res
