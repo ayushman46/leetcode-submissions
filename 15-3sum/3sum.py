0660683369e138e46bbe87902a1145d65a58d5f5
Ayushman <91052985+ayushman46@users.noreply.github.com>
2026-06-25 18:12:55 +0530

    Time: 651 ms (85.69%) | Memory: 18.2 MB (94.16%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        '''
+        
         res=[]
         nums.sort()
         res=[]
@@ -21,31 +21,6 @@ class Solution(object):
                 else:
                     res.append([a,nums[l],nums[r]])
                     l+=1
-                    if nums[l]==nums[l-1] and l<r:
+                    while nums[l]==nums[l-1] and l<r:
                         l+=1
-        return res'''
-        res = []
-        nums.sort()
-
-        for i, a in enumerate(nums):
-            if a > 0:
-                break
-
-            if i > 0 and a == nums[i - 1]:
-                continue
-
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-
         return res
