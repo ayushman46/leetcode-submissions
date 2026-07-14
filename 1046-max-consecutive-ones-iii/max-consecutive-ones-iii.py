eacc0bf5754f6b9c601c0e281a711aa313eb4ab2
Ayushman <91052985+ayushman46@users.noreply.github.com>
2026-07-15 02:25:42 +0530

    Time: 97 ms (53.60%) | Memory: 15.8 MB (55.02%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -7,17 +7,13 @@ class Solution(object):
         """
         l=0
         count=0
-        res=0
+        maxl=0
         for r in range(len(nums)):
             if nums[r]==0:
                 count+=1
-            
             while count>k:
                 if nums[l]==0:
                     count-=1
                 l+=1
-            res=max(res,r-l+1)
-        return res
-
-
-            
+            maxl=max(maxl,r-l+1)
+        return maxl
