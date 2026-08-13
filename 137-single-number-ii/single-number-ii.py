e9016c6e9c882d2bb9e01bceeba8219c12401daf
Ayushman <91052985+ayushman46@users.noreply.github.com>
2026-08-13 19:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (23.71%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a={}
+        for i in nums:
+            if i in a:
+                a[i]+=1
+            else:
+                a[i]=1
+        for i in a:
+            if a[i] == 1:
+                return i
